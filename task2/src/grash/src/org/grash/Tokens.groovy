89b953192a8d8a64608c0d1020f1f435e3ee6cb0
Jakob Gruber <jakob.gruber@gmail.com>
2013-05-23 20:14:10 +0200

    Relax definition for VAL tokens
    Values muest be able to include ':' characters in order to be able to
    call ourselves.

--- task2/src/grash/src/org/grash/Tokens.groovy
+++ task2/src/grash/src/org/grash/Tokens.groovy
@@ -6,7 +6,7 @@ class Tokens {
     static final WHITE_ = '[ ]*'
     static final SYM = '[a-zA-Z0-9]+'
     static final REGEXP = '[^:]+'
-    static final VAL = '[^:]+'
+    static final VAL = '.+'
     static final PATH = '[^:]+'
     static final ACTION = /(prm|seq|alt|set)\(.+\)/
     static final COMMENT = '#.*'
